549f71ec18a6be4c5c27c240ce052bba21400995
RMartires <rohitmartires14@gmail.com>
2020-04-01 13:52:44 +0530

    added topojson

--- src/Components/Map.js
+++ src/Components/Map.js
@@ -4,7 +4,7 @@ import { ComposableMap, Geographies, Geography, ZoomableGroup } from "react-simp
 import './Maps.css';
 import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
-import Col from 'react-bootstrap
+import Col from 'react-bootstrap/Col'
 
 const geoUrl = "https://raw.githubusercontent.com/deldersveld/topojson/master/countries/india/india-districts.json";
 var temp1, temp2;
@@ -13,9 +13,9 @@ var states = [], districts = [];
 
 
 const Maps = (props) => {
-   const [position, setPosition] = useState({ coordinates: [80, 22], zoom: 9 });
-    const [selectedState, setSelectedState] = useState();
-    const [selectedDistrict, setSelectedDistrict] = useState();
+  const [position, setPosition] = useState({ coordinates: [80, 22], zoom: 9 });
+  const [selectedState, setSelectedState] = useState();
+  const [selectedDistrict, setSelectedDistrict] = useState();
   function handleZoomIn() {
     if (position.zoom >= 16) return;
     setPosition(pos => ({ ...pos, zoom: pos.zoom * 2 }));
@@ -47,112 +47,112 @@ const Maps = (props) => {
     var h = (1.0 - value) * 240
     return "hsl(" + h + ", 100%, 50%)";
   }
-      // selection of districts //
-     
-    // selection of districts //
+  // selection of districts //
+
+  // selection of districts //
   return (
-   <div className="main">
-       <Container>
-         <Row>
-           <Col sm={8}>
-      <div className="map">
-      {gethighest()}
-      <ComposableMap
-        width={1000}
-        fill="#D6D6DA"
-        stroke="#FFFFFF"
-        strokeWidth={0.1}
-      >
-        <ZoomableGroup
-          zoom={position.zoom}
-          center={position.coordinates}
-          onMoveEnd={handleMoveEnd}>
-          <Geographies geography={geoUrl}>
-            {({ geographies }) =>
-              geographies.map(geo => {
-                var state = geo.properties.NAME_1;
-                var dist = geo.properties.VARNAME_2;
+    <div className="main">
+      <Container>
+        <Row>
+          <Col sm={8}>
+            <div className="map">
+              {gethighest()}
+              <ComposableMap
+                width={1000}
+                fill="#D6D6DA"
+                stroke="#FFFFFF"
+                strokeWidth={0.1}
+              >
+                <ZoomableGroup
+                  zoom={position.zoom}
+                  center={position.coordinates}
+                  onMoveEnd={handleMoveEnd}>
+                  <Geographies geography={geoUrl}>
+                    {({ geographies }) =>
+                      geographies.map(geo => {
+                        var state = geo.properties.NAME_1;
+                        var dist = geo.properties.VARNAME_2;
 
-                var dist = geo.properties.NAME_2;
+                        var dist = geo.properties.NAME_2;
 
-                if (props.data) {
-                  temp1 = props.data[state];
-                }
-                if (temp1) {
-                  temp2 = temp1.districtData[dist];
-                }
-                if (temp1 && temp2) {
-                  // console.log(temp2);
-                  return < Geography
-                    key={geo.rsmKey}
-                    geography={geo}
-                    fill={heatMapColorforValue(temp2.confirmed)}
-                  />
-                } else {
-                  return < Geography
-                    key={geo.rsmKey}
-                    geography={geo}
-                  />
-                }
-              })
-            }
-          </Geographies>
-        </ZoomableGroup>
-      </ComposableMap>
-        </div>
-    </Col>        
- <Col sm={4}>
-    <div className="controls">
-      <div className="box">
-        <button onClick={handleZoomIn}>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 27"
-            stroke="currentColor"
-            strokeWidth="3"
-          >
-            <line x1="12" y1="4" x2="12" y2="19" />
-            <line x1="5" y1="12" x2="19" y2="12" />
-          </svg>
-        </button>
-        <button onClick={handleZoomOut}>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 27"
-            stroke="currentColor"
-            strokeWidth="3"
-          >
-         <line x1="5" y1="12" x2="19" y2="12" />
-          </svg>
-        </button>
-        </div>
+                        if (props.data) {
+                          temp1 = props.data[state];
+                        }
+                        if (temp1) {
+                          temp2 = temp1.districtData[dist];
+                        }
+                        if (temp1 && temp2) {
+                          // console.log(temp2);
+                          return < Geography
+                            key={geo.rsmKey}
+                            geography={geo}
+                            fill={heatMapColorforValue(temp2.confirmed)}
+                          />
+                        } else {
+                          return < Geography
+                            key={geo.rsmKey}
+                            geography={geo}
+                          />
+                        }
+                      })
+                    }
+                  </Geographies>
+                </ZoomableGroup>
+              </ComposableMap>
+            </div>
+          </Col>
+          <Col sm={4}>
+            <div className="controls">
+              <div className="box">
+                <button onClick={handleZoomIn}>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 27"
+                    stroke="currentColor"
+                    strokeWidth="3"
+                  >
+                    <line x1="12" y1="4" x2="12" y2="19" />
+                    <line x1="5" y1="12" x2="19" y2="12" />
+                  </svg>
+                </button>
+                <button onClick={handleZoomOut}>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 27"
+                    stroke="currentColor"
+                    strokeWidth="3"
+                  >
+                    <line x1="5" y1="12" x2="19" y2="12" />
+                  </svg>
+                </button>
+              </div>
 
-        <div className="districtselector">
-           <p className="title"> Select the state</p>
-           <select className="select">
-        
-            <option>
-            
-            </option>
-           </select>
-           <p className="title"> Select the District</p>
-           <select className="select">
-        
-            <option>
-            
-            </option>
-           </select>
-       </div>
-      </div>
-      </Col>
-      </Row>
+              <div className="districtselector">
+                <p className="title"> Select the state</p>
+                <select className="select">
+
+                  <option>
+
+                  </option>
+                </select>
+                <p className="title"> Select the District</p>
+                <select className="select">
+
+                  <option>
+
+                  </option>
+                </select>
+              </div>
+            </div>
+          </Col>
+        </Row>
       </Container>
-      </div>
-        );
+    </div>
+  );
 };
 
 
